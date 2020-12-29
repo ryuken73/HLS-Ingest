06b69d0080b5c27e8df0df883fc73b6df0d009e1
ryuken <ryuken73@gmail.com>
2020-12-29 17:24:49 +0900

    redux: handle state of currently active source (live or clip)

---
+++ app/modules/activeSources.js
@@ -0,0 +1,38 @@
+import {createAction, handleActions} from 'redux-actions';
+ 
+const {getDefaultConfig} = require('../lib/getConfig');
+const config = getDefaultConfig();
+
+const {
+    NUMBER_OF_CHANNELS
+} = config;
+
+const channelActiveSource = new Map();
+for(let channelNumber=1;channelNumber<=NUMBER_OF_CHANNELS;channelNumber++){
+    channelActiveSource.set(channelNumber, 'clip');
+}
+
+// action types
+const SET_CHANNEL_ACTIVE_SOURCE = 'activeSources/SET_CHANNEL_ACTIVE_SOURCE';
+
+// action creator
+export const setChannelActiveSource = createAction(SET_CHANNEL_ACTIVE_SOURCE);
+
+const initialState = {
+    channelActiveSource
+}
+
+// reducer
+export default handleActions({
+    [SET_CHANNEL_ACTIVE_SOURCE]: (state, action) => {
+        // console.log('%%%%%%%%%%%%%%%%', action.payload);
+        const {channelNumber, sourceFrom} = action.payload;
+        const {channelActiveSource} = state;
+        channelActiveSource.set(channelNumber, sourceFrom);
+        const newActiveSource = new Map(channelActiveSource);
+        return {
+            ...state,
+            channelActiveSource: newActiveSource
+        }
+    }
+}, initialState);
