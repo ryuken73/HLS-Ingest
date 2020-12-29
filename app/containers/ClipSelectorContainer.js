57625c57726ea564044416eca2b80929293cc570
ryuken <ryuken73@gmail.com>
2020-12-29 17:31:15 +0900

    redux: apply redux on ClipSelector

---
+++ app/containers/ClipSelectorContainer.js
@@ -0,0 +1,35 @@
+// @flow
+import * as React from 'react';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import ClipSelector from '../components/HLSIngester/ClipSelector';
+import * as clipSelectorActions from '../modules/clipSelector';
+import * as activeSourcesActions from '../modules/activeSources';
+import * as hlsPlayersActions from '../modules/hlsPlayers';
+
+
+
+function mapStateToProps(state, ownProps) {
+  // console.log('mapStateToProps:',state) 
+  const {channelNumber} = ownProps;
+  const {hours, currentClips, selectedHour} = state.clipSelector;
+  const {channelActiveSource} = state.activeSources;
+  return {
+    ...ownProps,
+    hours,
+    savedClips: currentClips.get(channelNumber),
+    active: channelActiveSource.get(channelNumber) === 'clip',
+    selectedHour
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    ClipSelectorActions: bindActionCreators(clipSelectorActions, dispatch),
+    ActiveSourcesActions: bindActionCreators(activeSourcesActions, dispatch),
+    HLSPlayersActions: bindActionCreators(hlsPlayersActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClipSelector);
