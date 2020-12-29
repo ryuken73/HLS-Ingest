78b1ca38f8363bc57677043d089737a53aa01557
ryuken <ryuken73@gmail.com>
2020-12-29 17:28:22 +0900

    redux: apply redux

--- app/modules/index.js
+++ app/modules/index.js
@@ -1,4 +1,6 @@
 export { default as liveSelector } from './liveSelector';
+export { default as clipSelector } from './clipSelector';
+export { default as activeSources } from './activeSources';
 export { default as app } from './app';
 export { default as body } from './body';
 export { default as options } from './options';
